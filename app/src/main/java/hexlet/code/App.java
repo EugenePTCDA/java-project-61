655552450ac61f1093faa6c76f98f1881f00a631
eugene <eugene.dbv@gmail.com>
2024-05-24 21:42:56 +0300

    delete while and fix 'switch' App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -13,35 +13,31 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("Welcome to the Brain Games!");
-            System.out.println("Please enter the game number and press Enter.");
 
-            System.out.println("1 - Greet");
-            System.out.println("2 - Even");
-            System.out.println("3 - Calc");
-            System.out.println("4 - GCD");
-            System.out.println("5 - Progression");
-            System.out.println("6 - Prime");
-            System.out.println("0 - Exit");
-            System.out.print("Your choice: ");
+        System.out.println("Please enter the game number and press Enter.");
 
-            int choice = scanner.nextInt();
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
 
-            switch (choice) {
-                case 0 -> {
-                    System.out.println("Goodbye!");
-                    return;
-                }
-                case 1 -> Greet.sayHello();
-                case 2 -> Even.play();
-                case 3 -> Calc.play();
-                case 4 -> GCD.play();
-                case 5 -> Progression.play();
-                case 6 -> Prime.play();
-                default -> System.out.println("Invalid choice. Please try again.");
-            }
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 0 -> System.out.println("Goodbye!");
+            case 1 -> Greet.sayHello();
+            case 2 -> Even.play();
+            case 3 -> Calc.play();
+            case 4 -> GCD.play();
+            case 5 -> Progression.play();
+            case 6 -> Prime.play();
+            default -> System.out.println("No such game");
         }
+
 
     }
 }
