02a182fe3d75c0a05412bc6d983424074c4f603a
eugene <eugene.dbv@gmail.com>
2024-05-24 22:13:17 +0300

    delete magic number in Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -4,13 +4,14 @@ import hexlet.code.Engine;
 
 public class Even {
     private static final String START_PHRASE = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-
+    private static final int EVEN_MIN_NUMBER = 1;
+    private static final int EVEN_MAX_NUMBER = 1000;
     public static void play() {
         int roundCount = Engine.getRoundsCount();
         String[][] rounds = new String[roundCount][2];
 
         for (int i = 0; i < roundCount; i++) {
-            int number = Engine.randomNumber(1, 1000);
+            int number = Engine.randomNumber(EVEN_MIN_NUMBER, EVEN_MAX_NUMBER);
 
             rounds[i][0] = Integer.toString(number);
             rounds[i][1] = checkEven(number) ? "yes" : "no";
