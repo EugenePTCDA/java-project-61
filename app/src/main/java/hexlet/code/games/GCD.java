b6810cc092f7814b6a1d2ca40bee186f8ef532fe
eugene <eugene.dbv@gmail.com>
2024-05-24 22:15:28 +0300

    delete magic number in GCD.java

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -4,14 +4,15 @@ import hexlet.code.Engine;
 
 public class GCD {
     private static final String START_PHRASE = "Find the greatest common divisor of given numbers.";
-
+    private static final int GCD_MIN_NUMBER = 10;
+    private static final int GCD_MAX_NUMBER = 100;
     public static void play() {
         int roundCount = Engine.getRoundsCount();
         String[][] rounds = new String[roundCount][2];
 
         for (int i = 0; i < roundCount; i++) {
-            int num1 = Engine.randomNumber(10, 100);
-            int num2 = Engine.randomNumber(10, 100);
+            int num1 = Engine.randomNumber(GCD_MIN_NUMBER, GCD_MAX_NUMBER);
+            int num2 = Engine.randomNumber(GCD_MIN_NUMBER, GCD_MAX_NUMBER);
 
             rounds[i][0] = num1 + " " + num2;
             rounds[i][1] = Integer.toString(findGCD(num1, num2));
